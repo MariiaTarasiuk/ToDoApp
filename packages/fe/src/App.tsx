62a3ae5f98257c6c1f0004b18e51eec673c98298
Mariia Tarasiuk <maria.tarasiuck@gmail.com>
2021-05-21 23:26:00 +0300

    move to handler and add checkbox for status

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -13,6 +13,11 @@ const READ_TODOS = gql`
   }
 `;
 
+const CREATE_TODO = gql`
+mutation CreateTodo($text: String!){
+  createTodo(text: $text)
+}`
+
 const DELETE_TODO = gql`
   mutation RemoveTodo($id: String!){ 
     removeTodo(id: $id)
@@ -23,18 +28,35 @@ const App = () => {
 
   const { data, loading, error } = useQuery(READ_TODOS);
   const [delTodo] = useMutation(DELETE_TODO);
+  const [addTodo] = useMutation(CREATE_TODO);
+  let todoInput: HTMLInputElement;
 
   if (loading) return <p>loading...</p>;
   if (error) return <p>ERROR</p>;
   if (!data) return <p>Not found</p>;
 
+  const addHandler = (event: HTMLFormElement) => {
+    event.preventDefault();
+    addTodo({ variables: { text: todoInput.value } });
+    window.location.reload();
+  }
+
+  const removeHandler = (id: string) => {
+    delTodo({ variables: { id } }); window.location.reload();
+  }
+
   return (
     <div className="app">
+      <form onSubmit={(e) => addHandler(e as any)}>
+        <input type='text' placeholder="add new todo" ref={node => { todoInput = node as HTMLInputElement; }} />
+        <button type="submit">Add Todo</button>
+      </form>
       <ul>
         {data.todos.map((todo: any) =>
           <li key={todo.id}>
+            <input type="checkbox" name="status" id={todo.id} checked={todo.completed} />
             <span className={todo.completed ? "complete" : "open"}>{todo.text}</span>
-            <button onClick={() => { delTodo({ variables: { id: todo.id } }); window.location.reload(); }}>X</button>
+            <button onClick={() => removeHandler(todo.id)}>X</button>
           </li>
         )}
       </ul>
