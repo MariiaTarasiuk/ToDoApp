da2e11da63ca77dc0ec6c65477a0568ca43fa2db
Mariia Tarasiuk <maria.tarasiuck@gmail.com>
2021-05-21 22:52:33 +0300

    add apollo client and query todos

--- packages/fe/src/App.tsx
+++ packages/fe/src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
 
-function App() {
+const READ_TODOS = gql`
+  query todos{
+    todos {
+      id
+      text
+      completed
+    }
+  }
+`;
+
+const App = () => {
+
+  const { data, loading, error } = useQuery(READ_TODOS);
+
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
   return (
-    <div className="App">
+    <div className="app">
+      <ul>
+        {data.todos.map((todo: any) =>
+          <li key={todo.id}>
+            <span className={todo.completed ? "complete" : "open"}>{todo.text}</span>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
